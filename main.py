425fb99d84e3c9851a92014ddbf9445482089cf1
Alice Vitti Kageyama Ferreira <alicevkf@gmail.com>
2023-10-29 19:24:01 -0300

    main.py semifuncional

--- main.py
+++ main.py
@@ -11,19 +11,17 @@ from numpy import array, unique, savetxt
 #                                    Classes                                   #
 # ---------------------------------------------------------------------------- #
 
-class Particula(simple_sphere):
+class Individuo(simple_sphere):
     '''
-    Classe utilizada para representar cada átomo como uma esfera.
-    (Hard spheres model)
+    Classe utilizada para representar indivíduo no modelo.
     '''
-    def __init__(self, id:int, pos:vector, vel:vector, raio:float, massa:float,
-                tipo:str, cor:vector):
+    def __init__(self, pos:vector, vel:vector, raio:float, massa:float,
+                especie:str, saudavel:bool, cor:vector):
         super().__init__(pos=pos, radius=raio, color=cor)
-        self.color = cor
-        self.id = id
         self.vel = vel
         self.massa = massa
-        self.tipo = tipo
+        self.especie = especie
+        self.saudavel = saudavel
         self.vizinhos = []
 
 
@@ -48,139 +46,148 @@ def dist(r1:vector, r2:vector):
     return mag(r1-r2)
 
 
-def colElastica(p1:Particula, p2:Particula):
+def neutro(i1:Individuo, i2:Individuo):
     '''
-    Calcula e atualiza os vetores velocidade resultantes de cada partícula após
-    sua colisão, considerando uma colisão elástica.
+    Calcula e atualiza os vetores velocidade resultantes de cada indivíduo após
+    sua interação, considerando uma interação neutra.
 
     Args:
-        p1 (Particula): representa uma das partículas na colisão
-        p2 (Particula): representa a outra partícula
+        i1 (Individuo): representa um dos indivíduos na interação
+        i2 (Individuo): representa o outro indivíduo
 '''
-    v1, m1, x1 = p1.vel, p1.massa, p1.pos
-    v2, m2, x2 = p2.vel, p2.massa, p2.pos
+    v1, m1, x1 = i1.vel, i1.massa, i1.pos
+    v2, m2, x2 = i2.vel, i2.massa, i2.pos
     v1_ = v1 - ((2*m2)/(m1+m2))*(dot(v1-v2, x1-x2)/(mag2(x1-x2)))*(x1-x2)
     v2_ = v2 - ((2*m1)/(m1+m2))*(dot(v2-v1, x2-x1)/(mag2(x2-x1)))*(x2-x1)
-    print(f'\nColisão entre as partículas {p1.id} e {p2.id}!')
-    p1.vel, p2.vel = v1_, v2_
+    print(f'\nInteração entre {i1.especie} e {i2.especie}!')
+    i1.vel, i2.vel = v1_, v2_
 
 
-def colInelastica(p1:Particula, p2:Particula):
+def infeccao(infect:Individuo, sus:Individuo):
     '''
-    Calcula e atualiza os vetores velocidade resultantes da partícula após a
-    colisão, considerando uma reação e colisão inelástica.
+    Infecção de um indivíduo suscetível por um infectado.
 
     Args:
-        p1 (Particula): representa uma das partículas na colisão
-        p2 (Particula): representa a outra partícula
+        inf (Individuo): representa uma indivíduo infectado
+        sus (Individuo): representa um indivíduo suscetível
     '''
-    v1, m1, x1 = p1.vel, p1.massa, p1.pos
-    v2, m2, x2 = p2.vel, p2.massa, p2.pos
+    global propriedades
+    
+    neutro(infect, sus)
+    print(f'O {sus.especie} foi infectado pelo {infect.especie}! :(')    
+    sus.saudavel = False
+    sus.color = propriedades[sus.especie]['corInf']
+    
+
+def predacao(pred:Individuo, pres:Individuo):
+    '''
+    Calcula e atualiza os vetores velocidade resultantes dos indivíduos após a
+    interação, considerando a predação da presa.
+
+    Args:
+        pred (Individuo): representa o predador
+        pres (Individuo): representa a presa
+    '''
+    global indMortos
+    
+    v1, m1, x1 = pred.vel, pred.massa, pred.pos
+    v2, m2, x2 = pres.vel, pres.massa, pres.pos
     m_ = (m1+m2)/2
     v_ = (m1*v1+m2*v2)/m_
     x_ = (x1+x2)/2
-    print(f'\nColisão entre as partículas {p1.id} e {p2.id}!')
-    p1.vel, p1.pos = v_, x_
+    print(f'\nInteração entre {pred.especie} e {pres.especie}!')
+    print(f'O {pres.especie} foi comido pelo {pred.especie}! :(')
+    pred.vel, pred.pos = v_, x_
+    indMortos.append(pres)
 
 
-def reacao(p1:Particula, p2:Particula):
+def interacao(i1:Individuo, i2:Individuo):
     '''
-    Realiza a reação A + A --> B entre as partículas p1 e p2.
-
-    Args:
-        p1 (Particula): representa uma das partículas na reação
-        p2 (Particula): representa a outra particula
-    '''
-    global propProduto, pInativas, nReag, nProd
+    Define o tipo de interação a ocorrer entre os dois indivíduos.
     
-    colInelastica(p1, p2)
-    p1.radius = propProduto['raio']
-    p1.massa = propProduto['massa']
-    p1.tipo = propProduto['tipo']
-    p1.color = propProduto['cor']
-    pInativas.append(p2)
-    nReag -= 2
-    nProd += 1
-
-
-def colisao(p1:Particula, p2:Particula):
-    '''
-    Chama uma colisão elástica ou inelástica dependendo da probabilidade de
-    reação.
-
     Args:
-        p1 (Particula): representa uma das partículas na colisão
-        p2 (Particula): representa a outra particula
+        i1 (Individuo): representa um dos indivíduos na interação
+        i2 (Individuo): representa o outro indivíduo
     '''
     global probReacao
     
-    t1, t2 = p1.tipo, p2.tipo
-    condReacao = randint(0, 100) <= probReacao
-    condTipo = (t1 == t2 == 'A')
-    if condReacao and condTipo: reacao(p1, p2)
-    else: colElastica(p1, p2)
+    esp1, esp2 = i1.especie, i2.especie
+    # Condições - infeccção:
+    condEspecie = (esp1 == esp2)
+    condSus = (False and True) in [i.saudavel for i in (i1, i2)]
+    condInf = randint(0, 100) < propriedades[esp1]['taxaInf']
+    # Condições - predação:
+    c1, c2, c3 = ('Gato' and 'Rato'), ('Gato' and 'Coelho'), ('Leao' and 'Gato')
+    condEspecies = (c1 or c2 or c3) in [esp1, esp2]
+    pres = (i1 if ((esp1=='Gato' and esp2=='Leao') and (esp1!='Leao')) else i2)
+    pred = (i1 if (pres==i2) else i2)
+    condPred = randint(0,100) < propriedades[pres.especie]['taxaPred']
+    # Defininindo tipo de interação:
+    if (condEspecie and condSus and condInf): infeccao(i1, i2)
+    elif (condEspecies and condPred): predacao(pred, pres)
+    else: neutro(i1, i2)
     
     
-def atualizaVizinhos(p:Particula):
+def atualizaVizinhos(i:Individuo):
     '''
-    Atualiza a lista de partículas vizinhas a uma partícula.
+    Atualiza a lista de indivíduos próximos a um determinado indivíduo.
 
     Args:
-        p (Particula): particula a ter seus vizinhos recalculados
+        i (Individuo): Indivíduo a ter seus vizinhos recalculados
     '''
-    global pAtivas
+    global indVivos
     
-    p.vizinhos.clear()
-    x, r = p.pos, p.radius
-    for p2 in pAtivas:
-        x2 = p2.pos
-        if (dist(x, x2) <= r+2.5) and (p != p2): p.vizinhos.append(p2)
+    i.vizinhos.clear()
+    x, r = i.pos, i.radius
+    for i2 in indVivos:
+        x2 = i2.pos
+        if (dist(x, x2) <= r+2.5) and (i != i2): i.vizinhos.append(i2)
     
     
-def atualizaPos(p:Particula):
+def atualizaPos(i:Individuo):
     '''
-    Atualiza a posição de uma partícula.
+    Atualiza a posição de um indivíduo.
     '''
     global dt
 
-    p.pos += p.vel*dt
+    i.pos += i.vel*dt
     
     
-def colCheckPartPart(p1:Particula, p2:Particula):
+def colCheckIndInd(i1:Individuo, i2:Individuo):
     '''
-    Checa colisão entre duas partículas.
+    Checa colisão entre dois indivíduos.
 
     Args:
-        p1 (Particula): representa uma das partículas na colisão
-        p2 (Particula): representa a outra partícula
+        i1 (Individuo): representa um dos indivíduos na interação
+        i2 (Individuo): representa o outro indivíduo
     '''
     global dt
     
-    x1, v1, r1 = p1.pos, p1.vel, p1.radius
-    x2, v2, r2 = p2.pos, p2.vel, p2.radius
+    x1, v1, r1 = i1.pos, i1.vel, i1.radius
+    x2, v2, r2 = i2.pos, i2.vel, i2.radius
     distMinima = r1+r2
     distancia = dist(x1, x2)
     distancia_ = dist(x1+(v1*dt), x2+(v2*dt))
     if (distancia <= distMinima) and (distancia > distancia_):
-        colisao(p1, p2)
+        interacao(i1, i2)
 
 
-def colCheckPartParede(p:Particula):
+def colCheckIndParede(i:Individuo):
     '''
-    Checa colisão entre uma partícula e as paredes da caixa.
+    Checa colisão entre um indivíduo e as paredes da caixa.
 
     Args:
-        p (Particula): partícula a ser examinada
+        i (Individuo): indivíduo a ser examinada
     '''
     global dt, ladoCaixa
     
-    r = p.radius
-    pos = p.pos
-    pos_  = p.pos+(p.vel*dt)
+    r = i.radius
+    pos = i.pos
+    pos_  = pos+(i.vel*dt)
     if (abs(pos.x) >= (ladoCaixa/2-r)) and (abs(pos.x) < abs(pos_.x)):
-        p.vel.x = -p.vel.x
+        i.vel.x = -i.vel.x
     if (abs(pos.y) >= (ladoCaixa/2-r)) and (abs(pos.y) < abs(pos_.y)):
-        p.vel.y = -p.vel.y
+        i.vel.y = -i.vel.y
     
     
 # ---------------------------------- Visual ---------------------------------- #
@@ -197,99 +204,91 @@ def criaCaixa():
                   vector(d,-d,0), vector(-d,-d,0)])
 
 
-def criaParticulas():
+def criaIndividuos(especie:str):
     '''
-    Cria instâncias da classe Partícula ma lista de partículas ativas.
+    Cria instâncias da classe Individuo de uma determinada espécie na lista de
+    indivíduos vivos.
+
+    Args:
+        especie (str): espécie da população a ser criada
     '''
-    global pInicial, ladoCaixa, velLimite, propReagente, pAtivas
+    global propriedades, ladoCaixa, velLimite, indVivos
     
-    for num in range(pInicial):
+    numInicial = propriedades[especie]['numInicial']
+    raio, massa, cor = propriedades[especie]['raio'], \
+    propriedades[especie]['massa'], propriedades[especie]['cor']
+    for _ in range(numInicial):
         pos = [randrange(-ladoCaixa/2+1, ladoCaixa/2-1) for _ in range (2)]
         pos = vector(pos[0], pos[1], 0)
         vel = [randint(0, velLimite) for _ in range(2)]
         vel = vector(vel[0], vel[1], 0)
-        p = Particula(
-            num, pos, vel,
-            propReagente['raio'],
-            propReagente['massa'],
-            propReagente['tipo'],
-            propReagente['cor']
-        )
-        pAtivas.append(p)
+        saudavel = randint(0, 100) < propriedades[especie]['taxaInf']
+        p = Individuo( pos, vel, raio, massa, especie, saudavel, cor)
+        indVivos.append(p)
         
         
 def criaGraficos():
     '''
     Cria gráficos que acompanham a simulação.
     '''
-    global grafComp, grafAlt, velLimite, pAtivas, pInicial, listaConc
+    global grafComp, grafAlt, listaConc
     
-    # Gráfico 1:
-    grafico1 = graph(title='Distribuição de velocidades', width=grafComp,
-                    height=grafAlt, align='left', xmax=velLimite, ymax=8,
-                    xtitle='Velocidade', ytitle='Número de partículas')
-    histData = array([int(mag(p.vel)) for p in pAtivas])
-    histX, histY = unique(histData, return_counts=True)
-    histograma = gvbars(data=list(zip(histX, histY)),
-                        color=vector(1,1,0))
-    graficos.append(histograma)
-    
-    # Gráfico 2:
+    # Gráfico de concentração:
+    valMaximo = 65
     grafico2 = graph(title='Concentração', width=grafComp,
-                    height=grafAlt, align='left', ymax=pInicial,
-                    xtitle='Tempo (Frames)', ytitle='Número de partículas')
-    concReag = gcurve(data=list(zip(listaConc[0], listaConc[1])),
-                    color=vector(1,0,0), label='Reagente')
-    concProd = gcurve(data=list(zip(listaConc[0], listaConc[2])),
-                    color=vector(0,0,1), label='Produto')
-    graficos.extend([concReag, concProd])
-    
+                    height=grafAlt, align='left', ymax=valMaximo,
+                    xtitle='Tempo', ytitle='Número de indivíduos')
+    concRato = gcurve(data=list(zip(listaConc[0], listaConc[1])),
+                    color=vector(.5,.5,.5), label='Ratos')
+    concCoelho = gcurve(data=list(zip(listaConc[0], listaConc[2])),
+                    color=vector(1,1,1), label='Coelhos')
+    concGato = gcurve(data=list(zip(listaConc[0], listaConc[3])),
+                    color=vector(1,.5,0), label='Gatos')
+    concLeao = gcurve(data=list(zip(listaConc[0], listaConc[4])),
+                    color=vector(1,1,0), label='Leao')
+    graficos.extend([concRato, concCoelho, concGato, concLeao])
+
 
 def atualizaGraficos():
     '''
     Atualiza os gráficos que acompanham a simulação.
-
-    Args:
-        histograma (gvbars): curva do histograma de velocidade
     '''
-    global pAtivas, graficos, listaConc
+    global graficos, listaConc
     
-    # Gráfico 1:
-    histograma = graficos[0]
-    histData = array([int(mag(p.vel)) for p in pAtivas])
-    histX, histY = unique(histData, return_counts=True)
-    histograma.data = list(zip(histX, histY))
-    
-    # Gráfico 2:
-    concReag, concProd = graficos[1], graficos[2]
-    concReag.data = list(zip(listaConc[0], listaConc[1]))
-    concProd.data = list(zip(listaConc[0], listaConc[2]))
+    # Gráfico de concentração:
+    concRato, concCoelho, concGato, concLeao = graficos[0], graficos[1], \
+    graficos[2], graficos[3]
+    concRato.data = list(zip(listaConc[0], listaConc[1]))
+    concCoelho.data = list(zip(listaConc[0], listaConc[2]))
+    concGato.data = list(zip(listaConc[0], listaConc[3]))
+    concLeao.data = list(zip(listaConc[0], listaConc[4]))
     
     
 # --------------------------------- Simulação -------------------------------- #
 
-def delParticula(p:Particula):
+def delIndividuo(i:Individuo):
     '''
     Deleta partícula da simulação.
 
     Args:
-        p (Particula): partícula a ser deletada
+        i (Individuo): partícula a ser deletada
     '''
-    global pAtivas
+    global indVivos
     
-    p.visible = False
-    pAtivas.remove(p)
-    del p
+    i.visible = False
+    indVivos.remove(i)
+    del i
     
     
 def exportarDados():
     '''
     Exporta os dados do gráfico de concentração.
     '''
-    global listaConc, pInicial
+    global listaConc
     
-    dados = [i for i in zip(listaConc[0],listaConc[1],listaConc[2])]
-    savetxt(f'dados/dadosNum={pInicial}.csv', dados, delimiter=', ',
+    dados = [i for i in zip(listaConc[0], listaConc[1], listaConc[2],
+                            listaConc[3], listaConc[4])]
+    savetxt(f'dados/dadosSimulacao.csv', dados, delimiter=', ',
             fmt='% s')
     
     
@@ -306,51 +305,56 @@ def pararSimulacao():
 def step():
     '''
     Passo da simulação:
-        * Deleta todas as partículas inativas;
-        * Reseta a lista de particulas inativas;
+        * Deleta todos os indivíduos mortos;
+        * Reseta a lista de indivíduos mortos;
         * Atualiza gráficos;
+        * Atualiza posições dos indivíduos;
         * Atualiza vizinhos;
-        * Atualiza posições das partículas;
-        * Check de colisão partícula-partícula;
-        * Check de colisão partícula-parede
+        * Check de colisão entre dois indivíduos;
+        * Check de colisão indivíduo-parede
     '''
-    global pInativas, pAtivas
+    global indMortos, indVivos
     
-    # * Deleta todas as partículas inativas:
-    [delParticula(p) for p in pInativas]
-    # * Reseta a lista de particulas inativas
-    pInativas.clear()
+    # * Deleta todos os indivíduos mortos:
+    [delIndividuo(i) for i in indMortos]
+    # * Reseta a lista de indivíduos mortos:
+    indMortos.clear()
     # * Atualiza gráficos:
     atualizaGraficos()
-    # * Atualiza posições das partículas:
-    for p in pAtivas:
-        atualizaPos(p)
+    # * Atualiza posições dos indivíduos;
+    for i in indVivos:
+        atualizaPos(i)
     # * Atualiza vizinhos:
-        atualizaVizinhos(p)
-    # * Check de colisão partícula-partícula:
-        for p2 in p.vizinhos:
-            colCheckPartPart(p, p2)
-    # * Check de colisão partícula-parede:
-        colCheckPartParede(p)
+        atualizaVizinhos(i)
+    # * Check de colisão entre dois indivíduos;
+        for i2 in i.vizinhos:
+            colCheckIndInd(i, i2)
+    # * Check de colisão indivíduo-parede:
+        colCheckIndParede(i)
 
 
 def simulacao():
     '''
     Função responsável pela simulação completa.
     '''
-    global nReag, listaConc, nProd
+    global parar, listaConc, nRato, nCoelho, nGato, nLeao
     
     t = 0
     criaCaixa()
-    criaParticulas()
+    criaIndividuos('Rato')
+    criaIndividuos('Coelho')
+    criaIndividuos('Gato')
+    criaIndividuos('Leao')
     criaGraficos()
-    while (nReag!=0) and not parar:
+    while not parar:
         step()
         t += 1
         listaConc[0].append(t)
-        listaConc[1].append(nReag)
-        listaConc[2].append(nProd)
-    print('\nFim da simulação!')
+        listaConc[1].append(nRato)
+        listaConc[2].append(nCoelho)
+        listaConc[3].append(nGato)
+        listaConc[4].append(nLeao)
+    print('\n\nFim da simulação!')
 
 
 
@@ -362,45 +366,73 @@ def simulacao():
 #                            Parâmetros da simulação                           #
 # ---------------------------------------------------------------------------- #
 
+# Propriedades dos indivíduos:
+propriedades = {
+    'Rato' : {
+        'raio':.15,
+        'massa':1,
+        'especie':'Rato',
+        'cor':vector(.5,.5,.5),
+        'corInf':vector(0,1,0),
+        'taxaInf':15,
+        'taxaPred':60,
+        'numInicial':50
+    },
+
+    'Coelho' : {
+        'raio':.2,
+        'massa':2,
+        'especie':'Coelho',
+        'cor':vector(1,1,1),
+        'taxaInf':0,
+        'taxaPred':50,
+        'numInicial':60
+    },
+
+    'Gato' : {
+        'raio':.25,
+        'massa':3,
+        'especie':'Gato',
+        'cor':vector(1,.5,0),
+        'corInf':vector(0,.5,0),
+        'taxaInf':10,
+        'taxaPred':40,
+        'numInicial':30
+    },
+
+    'Leao' : {
+        'raio':.4,
+        'massa':5,
+        'especie':'Leao',
+        'cor':vector(1,1,0),
+        'corInf':vector(0,.2,0),
+        'taxaInf':5,
+        'taxaPred':0,
+        'numInicial':20
+    }
+}
+
 dt = 1e-3 # Variação no tempo em cada frame
 velLimite = 40 # Limite de velocidade inicial para as partículas
 
-probReacao = 40 # Probabilidade de reação (em %)
+nRato = propriedades['Rato']['numInicial'] # Número de indivíduos da espécie Rato
+nCoelho = propriedades['Coelho']['numInicial'] # Número de indivíduos da espécie Coelho
+nGato = propriedades['Gato']['numInicial'] # Número de indivíduos da espécie Gato
+nLeao = propriedades['Leao']['numInicial'] # Número de indivíduos da espécie Leão
 
-pInicial = 100 # Número de partículas inicial
-nReag = pInicial # Número de partículas de reagente
-nProd = 0 # Número de partículas de produto
-
-pAtivas = [] # Lista de partículas ativas
-pInativas = [] # Lista de partículas inativas
+indVivos = [] # Lista de indivíduos vivos
+indMortos = [] # Lista de indivíduos mortos
 
 ladoCaixa = 20 # Lado da caixa imaginária contendo a simulação
-
-# Propriedades das partículas:
-propReagente = {
-    'raio':0.1,
-    'massa':4e-23,
-    'tipo':'A',
-    'cor':vector(1,0,0)
-}
-
-propProduto = {
-    'raio':0.12,
-    'massa':6e-23,
-    'tipo':'B',
-    'cor':vector(0,0,1)
-}
 
 # Gráfico de concentração:
 graficos = []
 listaTempo = [0]
-listaReag = [nReag]
-listaProd = [nProd]
-listaConc = [listaTempo, listaReag, listaProd]
-
-# Gráfico de Maxwell-Boltzamann:
-k = 1.38e-23
-T = 24
+listaRato = [nRato]
+listaCoelho = [nCoelho]
+listaGato = [nGato]
+listaLeao = [nLeao]
+listaConc = [listaTempo, listaRato, listaCoelho, listaGato, listaLeao]
 
 
 # ---------------------------------------------------------------------------- #
